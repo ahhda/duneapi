273af5f97232bad63b4a5fb6cc751b01fcd04c34
Ben Smith <bh2smith@gmail.com>
2022-05-08 16:20:40 +0200

    change test data

--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -9,32 +9,32 @@ class TestMockDB(unittest.TestCase):
         cur = db_conn.cursor()
 
         table = """
-        CREATE TABLE student(
-            id SERIAL PRIMARY KEY, 
-            firstName VARCHAR(40) NOT NULL, 
-            lastName VARCHAR(40) NOT NULL, 
-            age INT, 
-            address VARCHAR(80), 
-            email VARCHAR(40)
-        )
+            CREATE TABLE student(
+                id SERIAL PRIMARY KEY, 
+                firstName VARCHAR(40) NOT NULL, 
+                lastName VARCHAR(40) NOT NULL, 
+                age INT, 
+                address VARCHAR(80), 
+                email VARCHAR(40)
+            )
         """
         values = [
-            "Mona the",
-            "Octocat",
-            9,
-            "88 Colin P Kelly Jr St, San Francisco, CA 94107, United States",
-            "octocat@github.com",
+            "Ben",
+            "Smith",
+            37,
+            "Berlin, Germany",
+            "bh2smith@gmail.com",
         ]
 
-        text = """
-        INSERT INTO student(firstname, lastname, age, address, email) 
-        VALUES(%s, %s, %s, %s, %s) RETURNING *
+        insert = """
+            INSERT INTO student(firstname, lastname, age, address, email) 
+            VALUES(%s, %s, %s, %s, %s) RETURNING *
         """.format(
             values
         )
 
         cur.execute(table)
-        cur.execute(text, values)
+        cur.execute(insert)
         cur.execute("SELECT * FROM student")
         x = cur.fetchall()
         self.assertEqual(1, len(x))
