f6ff82354ace1ce384ccd4cdf2fbff27320609ee
Ben Smith <bh2smith@gmail.com>
2022-05-20 15:48:46 +0200

    remove silly replace for null

--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -9,10 +9,34 @@ def data_mapper(item: str) -> str:
     if item.startswith("\\x"):
         return f"'{item}'::bytea"
     if not item:
-        return ""
+        return "NULL"
     if re.match(r"^\d*.\d*$", item):
         return item
     return f"'{item}'"
+
+
+def build_schema(cur) -> None:
+    with open("tests/build_schema.sql", "r", encoding="utf-8") as file:
+        populate_query = file.read()
+
+    cur.execute(populate_query)
+
+
+def insert_query(data_dir: str, filename: str) -> str:
+    schema, table_name = filename.replace(".csv", "").split(".")
+    table = f'{schema}."{table_name}"'
+    with open(os.path.join(data_dir, filename), "r") as dat_file:
+        reader = dat_file.readlines()
+        fields = reader[0]
+        values = []
+        for row in reader[1:]:
+            row_items = map(lambda t: data_mapper(t), row.split(","))
+            row_string = ",".join(row_items)
+            values.append(f"({row_string})")
+
+        value_string = ",\n".join(values)
+        return f"INSERT INTO {table} ({fields}) VALUES {value_string};"
+
 
 class TestMockDB(unittest.TestCase):
     def test_db_connect(self):
@@ -52,41 +76,21 @@ class TestMockDB(unittest.TestCase):
         x = cur.fetchall()
         self.assertEqual(1, len(x))
 
-    def test_db_connect(self):
+    def test_db_populate(self):
         db_conn = connect()
         cur = db_conn.cursor()
 
-        with open("tests/build_schema.sql", "r", encoding="utf-8") as file:
-            populate_query = file.read()
-
-        cur.execute(populate_query)
+        build_schema(cur)
 
         data_dir = "tests/data"
         for filename in os.listdir(data_dir):
-            schema, table_name = filename.replace(".csv", "").split(".")
-            table = f'{schema}."{table_name}"'
-
-            with open(os.path.join(data_dir, filename), "r") as dat_file:
-                reader = dat_file.readlines()
-                fields = reader[0]
-                values = []
-                for row in reader[1:]:
-                    row_items = map(lambda t: data_mapper(t), row.split(","))
-                    row_string = ",".join(row_items)
-                    values.append(f"({row_string})")
-                value_string = ','.join(values[:5]).replace(",,", ",NULL,")
-                insert_query = (
-                    f"INSERT INTO {table} ({fields}) VALUES {value_string};"
-                )
-            print("Inserting data", insert_query)
+            query = insert_query(data_dir, filename)
+            print("Inserting data", query)
             cur.execute(insert_query)
 
         cur.execute("SELECT * FROM erc20.tokens;")
         x = cur.fetchall()
         self.assertEqual(5, len(x))
-        for row in x:
-            print(str(row[0]))
-        self.assertEqual(1, 0)
 
 
 if __name__ == "__main__":
