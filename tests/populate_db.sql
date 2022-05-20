811a5adef7a8c1ca4184d866d8c21557f8ef4ab4
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-05-20 22:57:27 +0200

    Mount entire workspace

--- tests/populate_db.sql
+++ tests/populate_db.sql
@@ -174,43 +174,43 @@ CREATE TABLE gnosis_protocol_v2.view_solvers (
 
 -- Copy CSV data into tables
 COPY erc20.tokens(contract_address, symbol, decimals)
-FROM '/csv/erc20.tokens.csv'
+FROM '/repo/tests/data/erc20.tokens.csv'
 DELIMITER ','
 CSV HEADER;
 
 
 COPY erc20."ERC20_evt_Transfer"
-FROM '/csv/erc20.ERC20_evt_Transfer.csv'
+FROM '/repo/tests/data/erc20.ERC20_evt_Transfer.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY prices.usd
-FROM '/csv/prices.usd.csv'
+FROM '/repo/tests/data/prices.usd.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY prices.prices_from_dex_data
-FROM '/csv/prices.prices_from_dex_data.csv'
+FROM '/repo/tests/data/prices.prices_from_dex_data.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY prices.layer1_usd_eth
-FROM '/csv/prices.layer1_usd_eth.csv'
+FROM '/repo/tests/data/prices.layer1_usd_eth.csv'
 DELIMITER ','
 CSV HEADER;
 
 
 COPY gnosis_protocol_v2.trades
-FROM '/csv/gnosis_protocol_v2.trades.csv'
+FROM '/repo/tests/data/gnosis_protocol_v2.trades.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY gnosis_protocol_v2.batches
-FROM '/csv/gnosis_protocol_v2.batches.csv'
+FROM '/repo/tests/data/gnosis_protocol_v2.batches.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY gnosis_protocol_v2.view_solvers
-FROM '/csv/gnosis_protocol_v2.view_solvers.csv'
+FROM '/repo/tests/data/gnosis_protocol_v2.view_solvers.csv'
 DELIMITER ','
 CSV HEADER;
